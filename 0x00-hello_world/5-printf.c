5957a0687d684cb9fe41e5ea908970a055253f2c
Isaac <isaacchimsomeje@gmail.com>
2022-06-30 16:55:47 -0700

    hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art'
+ * return - 0 if exited properly, else is not 0
+ */
+{
+	printf("with proper grammar, but the outcome is a piece of art"\n)
+	return (0);
+}
