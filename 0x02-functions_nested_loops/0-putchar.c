2a19178ff8a835ee4040f34295f289364a9cc316
Isaac <isaacchimsomeje@gmail.com>
2022-07-05 16:58:50 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,19 +2,12 @@
 
 /**
  * main - prints _putchar, followed by a new line
- * alx - holds the main printf statement
  * Return: 0 if successful, else not 0
  */
 
-void alx(void);
-
-void alx(void)
-{
-	printf("_putchar\n");
-}
 
 int main(void)
 {
-	alx();
+	printf("_putchar\n");
 	return (0);
 }
