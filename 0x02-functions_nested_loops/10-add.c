cd19ce819f790778dd5f9c07c57bdb32e38e92d5
Isaac <isaacchimsomeje@gmail.com>
2022-07-05 19:10:18 -0700

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - returns the sum of two integers
+ * @n: int type 1
+ * @m: int type 2
+ * Return: 1, sum of n and m
+ */
+
+int add(int n, int m)
+{
+	int a = n;
+	int b = m;
+	int c = n = m;
+
+	return (c);
+}
