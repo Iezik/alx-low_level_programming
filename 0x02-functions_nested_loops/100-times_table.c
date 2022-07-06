3972659471daca9e9dfc3dd62cfdffc0f2951567
Isaac <isaacchimsomeje@gmail.com>
2022-07-05 19:50:58 -0700

    fibonacci party

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,43 +1,49 @@
 #include "main.h"
 
 /**
- * times_table - prints the times table from 0 to 9
- * Return: void
+ * print_times_table - prints times table
+ * @n : times table to use
+ * Return:void
  */
 
-void times_table(int n)
+void print_times_table(int n)
 {
-	int a = 0;
-	int b;
-	int rep;
 
-	while (a <= 9)
+int a = 0, rep, b;
+
+if (n < 0 || n > 15)
+	return;
+
+while (a <= n)
+{
+	for (b = 0; b <= n; b++)
 	{
-		b = 0;
-		while (b <= 9)
-		{
 		rep = a * b;
 		if (b == 0)
-		{
 			_putchar('0' + rep);
-		}
 		else if (rep < 10)
 		{
 			_putchar(' ');
+			_putchar(' ');
 			_putchar('0' + rep);
 		}
-		else
+		else if (rep < 100)
 		{
+			_putchar(' ');
 			_putchar('0' + rep / 10);
 			_putchar('0' + rep % 10);
 		}
-
+		else
+		{
+			_putchar('0' + rep / 100);
+			_putchar('0' + (rep - 100) / 10);
+			_putchar('0' + rep % 10);
+		}
 		if (b < n)
 		{
 			_putchar(',');
 			_putchar(' ');
 		}
-		b++;
 	}
 	_putchar('\n');
 	a++;
