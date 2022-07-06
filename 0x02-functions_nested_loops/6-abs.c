f15eabb0402ee3ba08ff620a10e68f04d17eba95
Isaac <isaacchimsomeje@gmail.com>
2022-07-05 18:11:54 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absilute value of an int
+ * @n : identifier for integer
+ * Return: 0 on success, else not 0
+ */
+
+int _abs(int)
+{
+
+	return (n >= 0 ? n : n * -1);
+}
