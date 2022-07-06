d635a5c41ba35052b3422ae1e1d9afe1d2170f24
Isaac <isaacchimsomeje@gmail.com>
2022-07-05 19:25:26 -0700

    n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print from n - 98
+ * @n : starting point
+ * Return: 0 or 1
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		printf("%i, ", n);
+		n++;
+	}
+	if (n >= 98)
+	{
+		printf("%i, ", n);
+		n--;
+	}
+	putchar('\n');
+}
