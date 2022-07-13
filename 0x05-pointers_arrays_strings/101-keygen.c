83018ac55076fc0834d8c7fc2e7926bc74432124
Isaac <isaacchimsomeje@gmail.com>
2022-07-12 21:27:37 -0700

    update

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -29,11 +29,11 @@ int main(void)
 	rando2 = rand();
 	rando3 = rand();
 
-	chat = rando / rando2;
-	chat2 = rando % rando2;
+	chat = rando % 123;
+	chat2 = (rando + rando2) % 123;
 	chat3 = chat2 + chat;
 	che = chat + chat2 + chat3;
-	che2 = rando2 % rando;
+	che2 = rando2 % 123;
 	che3 = chat3 + chat;
 	che4 = chat3 + chat2;
 	che5 = chat + chat3 + chat2 + chat3;
@@ -43,6 +43,6 @@ int main(void)
  * desired length by reducing the numerica value
  * The modulus operator becomes pretty handy here 
  */
-	printf("%lu%c%lu%c%c%lu%c%c\n", rando % (maxvalue + 1), che, rando2 % (maxvalue + 1), che2, che3, rando3 % (maxvalue + 1), che4, che5);
+	printf("%lu%c%c%lu%c%c%lu%c%c\n", rando % (maxvalue + 1), che, che5, rando2 % (maxvalue + 1), che2, che3, rando3 % (maxvalue + 1), che4, che5);
 	return (0);
 }
