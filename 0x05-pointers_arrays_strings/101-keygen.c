ae35d2f66c5269d067bf2f5fd01bb6ca6125f19d
Isaac <isaacchimsomeje@gmail.com>
2022-07-12 21:19:40 -0700

    update

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,27 +13,36 @@ int main(void)
 {
 	long int rando;
 	long int rando2;
-	long int maxvalue = 716287492749186;
+	long int rando3;
+	long int maxvalue = 794;
 	int chat;
 	int chat2;
 	int chat3;
 	char che;
+	char che2;
+	char che3;
+	char che4;
+	char che5;
 
 	srand(time(NULL));
 	rando = rand();
 	rando2 = rand();
-	
+	rando3 = rand();
 
 	chat = rando / rando2;
 	chat2 = rando % rando2;
 	chat3 = chat2 + chat;
 	che = chat + chat2 + chat3;
+	che2 = rando2 % rando;
+	che3 = chat3 + chat;
+	che4 = chat3 + chat2;
+	che5 = chat + chat3 + chat2 + chat3;
 
 /**
  * The following code shortens the length of the generated number to a
  * desired length by reducing the numerica value
  * The modulus operator becomes pretty handy here 
  */
-	printf("%lu%c%lu\n", rando % (maxvalue + 1), che, rando2);
+	printf("%lu%c%lu%c%c%lu%c%c\n", rando % (maxvalue + 1), che, rando2 % (maxvalue + 1), che2, che3, rando3 % (maxvalue + 1), che4, che5);
 	return (0);
 }
