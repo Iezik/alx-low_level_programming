5c189960394a2a1c2958b1d7d64494f6144d30f1
Isaac <isaacchimsomeje@gmail.com>
2022-08-15 20:13:17 +0100

    Corrected prototype for printerlindlist

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,18 +11,15 @@
 
 size_t print_listint(const listint_t *h)
 {
-	//ptr is a pointer we will use to run traverse, print, and count the linked list
 	const listint_t *ptr;
 	ptr = malloc(sizeof(listint_t));
 	ptr = h;
-	
-	//count is used to store the number of nodes in the linked list
+
 	size_t count = 0;
 
 	while (ptr->next != NULL)
 	{
 		printf("%d\n", ptr->n);
-		count++;
 		ptr = ptr->next;
 	}
 	free(ptr);
