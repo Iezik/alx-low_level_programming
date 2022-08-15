57bacb25a5aa1cf4f7d93423ee2e7ab7ab07b1ff
Isaac <isaacchimsomeje@gmail.com>
2022-08-16 00:13:42 +0100

    more edits to printlinkedlist

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,16 +11,14 @@
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *ptr;
-	ptr = malloc(sizeof(listint_t));
 	ptr = h;
 	size_t count = 0;
 
-	while (ptr != NULL)
+	while (ptr->next != NULL)
 	{
 		printf("%d\n", ptr->n);
 		ptr = ptr->next;
 		count++;
 	}
-	free(ptr);
 return(count);
 }
